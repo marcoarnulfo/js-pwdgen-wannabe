c7fe1528cd1a467af6aa1e391e814d46bf6eec66
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-05 13:52:45 +0200

    end pwd generator

---
+++ assets/JS/main.js
@@ -0,0 +1,11 @@
+//Chiedi all'utente il suo nome
+const userName = prompt("Inserisci Nome")
+//Chiedi all'utente il suo cognome
+const userLastName = prompt("Inserisci Cognome")
+//Chiedi all'utente il suo colore preferito
+const userColor = prompt("Inserisci Colore preferito")
+//Inserisci l'anno corrente
+const currentYear = 2022
+//Genera la password per l'utente
+const password = userName + userLastName + userColor + currentYear 
+document.getElementById("pw").innerHTML = password
