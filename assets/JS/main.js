535fbdd8b0220fbe5683d6f0c5178b163245e286
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-06 09:34:45 +0200

    add ;

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,11 +1,11 @@
 //Chiedi all'utente il suo nome
-const userName = prompt("Inserisci Nome")
+const userName = prompt("Inserisci Nome");
 //Chiedi all'utente il suo cognome
-const userLastName = prompt("Inserisci Cognome")
+const userLastName = prompt("Inserisci Cognome");
 //Chiedi all'utente il suo colore preferito
-const userColor = prompt("Inserisci Colore preferito")
+const userColor = prompt("Inserisci Colore preferito");
 //Inserisci l'anno corrente
-const currentYear = 2022
+const currentYear = 2022;
 //Genera la password per l'utente
-const password = userName + userLastName + userColor + currentYear 
-document.getElementById("pw").innerHTML = password
+const password = userName + userLastName + userColor + currentYear ;
+document.getElementById("pw").innerHTML = password;
